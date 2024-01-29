7c61d413b0bbcfe72f999ac25e81d06af33b2f2c
wángzhìqiáng <40115555+zzqqw@users.noreply.github.com>
2024-01-29 11:52:58 +0800

    install.sh && docs

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# palworld
+#### **部署环境**
+
+- 服务器套餐配置：以CPU 4核、内存 16GB为例（通常可以满足6-8人同时在线联机）
+- 操作系统：Ubuntu 22.04 LTS
+
+#### 运行一键部署命令
+
+登陆服务器执行shell命令进行幻兽帕鲁的部署。
+
+```shell
+wget -O - https://raw.githubusercontent.com/zzqqw/palworld-install/main/install.sh|sh
+或
+wget -O - https://cdn.jsdelivr.net/gh/zzqqw/palworld-install@main/install.sh|sh
+```
+
+> **⚠️**注意
+>
+> 1. 有可能因为网络原因导致脚本运行失败，建议您多次重试或换个时间段再次部署。此处的原因主要是安装过程中需要请求Steam的服务器，网络连接状况可能不稳定。
+>
+> 2. 非root账号执行
+
+一键部署脚本执行成功后，会在服务器中创建一个名为`pal-server`的服务，您可以使用如下命令检查服务是否正常启动，如出现图示的效果，则证明服务器已经部署成功。
+
+```shell
+sudo systemctl status pal-server
+```
+
+如果您后续想管理该服务，可以参考以下命令来进行：
+
+~~~
+# 启动幻兽帕鲁的服务
+sudo systemctl start pal-server
+# 重启幻兽帕鲁的服务
+sudo systemctl restart pal-server
+# 关闭幻兽帕鲁的服务
+sudo systemctl stop pal-server
+# 查询幻兽帕鲁服务的状态
+sudo systemctl status pal-server
+~~~
+
+#### 开放端口8211
+
+幻兽帕鲁默认使用8211端口进行通信，进入服务商放通8211端口，协议UDP
+
+#### 配置项
+1、复制并执行以下命令为配置文件增加权限，避免后续步骤中由于权限问题导致无法编辑。
+
+~~~
+sudo chmod 777 /home/steam/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini
+~~~
+
+2、使用如下命令，将默认配置文件复制到幻兽帕鲁下的LinuxServer目录中：
+
+默认情况下，PalWorldSettings.ini文件是空的，手动配置的门槛很高，因此我们推荐在默认配置文件的基础上进行修改。
+
+~~~
+sudo cp /home/steam/Steam/steamapps/common/PalServer/DefaultPalWorldSettings.ini /home/steam/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini
+~~~
+
+3、使用如下命令打开游戏参数的配置文件：PalWorldSettings.ini。
+
+~~~
+sudo vim /home/steam/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini
+~~~
+
+
+
---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+if [ "$(whoami)" = "root" ]; then
+  echo "Error: Running as root user is not allowed" >&2
+  exit 1
+fi
+
+steam_user=steam
+if getent passwd "$steam_user" >/dev/null 2>&1; then
+    echo "User $steam_user exists."
+else
+    echo "User $steam_user does not exist.Adding $steam_user ..."
+    sudo useradd -m -s /bin/bash $steam_user
+fi
+
+echo "Installing SteamCMD..."
+
+sudo add-apt-repository multiverse -y
+sudo dpkg --add-architecture i386
+sudo apt update -y
+sudo apt-get remove needrestart -y
+
+echo steam steam/license note '' | sudo debconf-set-selections 
+echo steam steam/question select "I AGREE" | sudo debconf-set-selections 
+sudo apt install steamcmd -y
+
+steam_user_path=~steam
+steamcmd_path=$(which steamcmd)
+
+if [ -z "$steamcmd_path" ]; then
+    echo "Error: Install SteamCMD failed"
+    exit 1
+else
+    echo "Install SteamCMD successfully"
+fi
+
+sudo -u $steam_user mkdir -p $steam_user_path/.steam/sdk64/
+echo "Downloading palServer..."
+sudo -u $steam_user $steamcmd_path +login anonymous +app_update 1007 validate +quit
+sudo -u $steam_user $steamcmd_path +login anonymous +app_update 2394010 validate +quit
+
+sudo cp $steam_user_path/Steam/steamapps/common/Steamworks\ SDK\ Redist/linux64/steamclient.so $steam_user_path/.steam/sdk64/
+
+systemd_unit=pal-server
+cat <<EOF > $systemd_unit.service
+[Unit]
+Description=$systemd_unit.service
+
+[Service]
+Type=simple
+User=$steam_user
+Restart=on-failure
+RestartSec=30s
+ExecStart=$steam_user_path/Steam/steamapps/common/PalServer/PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo mv $systemd_unit.service /usr/lib/systemd/system/
+echo "Starting palServer..."
+sudo systemctl enable $systemd_unit
+sudo systemctl restart $systemd_unit
+sudo systemctl -l --no-pager status $systemd_unit
+
+if systemctl --quiet is-active "$systemd_unit"
+then
+    echo -e "\nPalServer is running successfully, enjoy!"
+else
+    echo -e "\nThere were some problems with the installation, please check the log."
+fi
+
+echo "Installing zram..."
+sudo apt-get install zram-config -y
+
+sleep 1
+sudo systemctl start zram-config.service
